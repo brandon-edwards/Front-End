f89dbd635cf5d822df4d8740591a3c58db13ade1
Edwards, Brandon <brandon.edwards@intel.com>
2023-06-26 15:48:36 -0700

    incorporating docker build changes into buildscript.sh, making BraTSPipline executible inside the container, still working to resolve s paramter type error

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,6 @@ FROM cbica/captk_centos7:devtoolset-4_superbuild
 
 LABEL authors="FeTS_Admin <admin@fets.ai>"
 
-ENV HTTPS_PROXY http://proxy-us.intel.com:912
-ENV HTTP_PROXY http://proxy-us.intel.com:912
-ENV http_proxy http://proxy-us.intel.com:912
-ENV https_proxy http://proxy-us.intel.com:912
-
 RUN yum update -y
 
 RUN yum install git
@@ -24,8 +19,6 @@ RUN bash ./buildscript.sh
 # set up the docker for GUI
 ENV QT_X11_NO_MITSHM=1
 ENV QT_GRAPHICSSYSTEM="native"
-
-RUN bash ./add_env/additional_environment.sh
 
 # define entry point
 # ENTRYPOINT ["/Front-End/bin/install/appdir/usr/bin/BraTSPipeline"]
--- buildscript.sh
+++ buildscript.sh
@@ -11,3 +11,23 @@ cmake -DCMAKE_INSTALL_PREFIX="./install/appdir/usr" -DITK_DIR="/work/CaPTk/bin/I
 
 echo "Running make + install"
 make -j4 && make install/strip
+
+echo "Installing Python3.8 and virtual environment for applications in /Front-End/src/applications"
+yum -y install epel-release
+yum -y update
+# watned a reboot here
+yum -y groupinstall “Development Tools”
+yum -y install openssl-devel bzip2-devel libffi-devel xz-devel
+
+yum -y install wget
+wget https://www.python.org/ftp/python/3.8.16/Python-3.8.16.tgz
+tar xvf Python-3.8.16.tgz
+cd Python-3.8*/
+./configure --enable-optimizations
+sudo make altinstall
+
+cd /Front-End/src/applications
+chmod +x BraTSPipeline.cxx
+python3.8 -m venv fetsapp_venv
+/Front-End/src/applications/fetsapp_venv/bin/pip install -e .
+
